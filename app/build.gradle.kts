bd52bd7ce31a539e14a2c8beefc40638d8a53b6a
LundiNord <git@nyxnord.de>
2026-03-03 09:38:46 +0100

    chore: version 1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "de.nyxnord.kraftlog"
         minSdk = 36
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
